538cec7a205a535e2dc93d8501f6a04d0610421b
rleathart <56587304+rleathart@users.noreply.github.com>
2021-06-02 14:25:29 +0100

    Add Check based testing

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Compile with
 cmake -B build -G Ninja -DDOTNET_PIPES=1
 cmake --build build
 ```
+
+Requires the [Check](https://github.com/libcheck/check) testing library.
--- test/pipe.c
+++ test/pipe.c
@@ -2,22 +2,34 @@
 #include <stdlib.h>
 
 #include <ipc/pipe.h>
+#include <check.h>
+
+START_TEST(test_pipe_create_and_destroy) {
+#ifdef _WIN32
+  char *pipename = "\\\\.\\pipe\\pipe.c.test_pipe";
+#else
+  char *pipename = "pipe.c.test_pipe";
+#endif
+  ipcError err = 0;
+  Pipe pipe = pipe_create(pipename, &err);
+  fail_if(err, "Could not create a simple pipe");
+  err = pipe_destroy(&pipe);
+  fail_if(err, "Could not destroy a pipe");
+}
+END_TEST
 
 int main(int argc, char **argv) {
-  ipcError err;
-  Pipe pipe;
+    Suite *s1 = suite_create("Pipe");
+    TCase *tc1_1 = tcase_create("Pipe");
+    SRunner *sr = srunner_create(s1);
+    int nf;
 
-  char *pipename = "pipe.c.test_pipe";
-  err = pipe_create(&pipe, pipename);
-  if (err) {
-    fprintf(stderr, "Error: %s\n", ipcError_str(err));
-    return EXIT_FAILURE;
-  }
+    suite_add_tcase(s1, tc1_1);
+    tcase_add_test(tc1_1, test_pipe_create_and_destroy);
 
-  err = pipe_destroy(&pipe);
-  if (err) {
-    fprintf(stderr, "Error: %s\n", ipcError_str(err));
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
+    srunner_run_all(sr, CK_ENV);
+    nf = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return nf == 0 ? 0 : 1;
 }
